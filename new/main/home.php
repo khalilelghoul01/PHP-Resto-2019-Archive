d9987cfc5c6e02b7f7174ada0aa576dbc0725f33
khalilelghoul01 <khalilelghoul01@gmail.com>
2019-08-05 03:26:34 +0200

    update

--- new/main/home.php
+++ new/main/home.php
@@ -9,7 +9,7 @@ include("../menu.php");
 	<?php include("../includes/header.php");?>
 	<?php if(!isset($_SESSION['user_email'])){ ?>
 	<img src="../images/bg.jpg" class="bg-image" id="responsive">
-	<div class="centered" id="phone"><button onclick="scrollWin()" id="btn" type="button" style="width: 150px;height: 40px;z-index: 1;" class="btn btn-outline-light"><i class="material-icons">keyboard_arrow_down</i></button></div>
+	<div class="centered" id="phone"><button onclick="scrollWin()" id="btn" type="button" style="width: 60px;height: 60px;z-index: 1;border-radius: 50%;" class="btn btn-outline-light"><i class="material-icons">keyboard_arrow_down</i></button></div>
 
 
 
@@ -98,7 +98,7 @@ getmenu();
 	function scrollWin() {
 	  window.scrollBy(0, y);
 	}
-	function detectmob() { 
+	function detectmob() {
 	 if( navigator.userAgent.match(/Android/i)
 	 || navigator.userAgent.match(/webOS/i)
 	 || navigator.userAgent.match(/iPhone/i)
@@ -116,15 +116,18 @@ getmenu();
 	  	var x = "40vh";
 	  	document.getElementById("responsive").style.height = x;
 	  	document.getElementById("phone").style.bottom = "48%";
-	  	document.getElementById("btn").style.height = "30px";
-	  	document.getElementById("btn").style.width= "70px";
-	  	document.getElementById("phone").style.left= "40px";
+	  	document.getElementById("btn").style.height = "50px";
+	  	document.getElementById("btn").style.width= "50px";
+	  	document.getElementById("phone").style.left= "50%";
 	  }
 	  else {
 	  	document.getElementById("responsive").style.height = "80vh";
+	  	document.getElementById("btn").style.height = "50px";
+	  	document.getElementById("btn").style.width= "50px";
+	  	document.getElementById("phone").style.bottom= "8%";
 	  }
 	}
-	
+	 
 	detectmob();
 </script>
 </body>
